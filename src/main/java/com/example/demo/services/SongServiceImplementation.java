e62596acf6eef5f5403712516c9a7a67d1bce0e0
Santhosh S <104092014+santhosh7349@users.noreply.github.com>
2024-01-31 22:34:14 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/services/SongServiceImplementation.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Song;
+import com.example.demo.repositories.SongRepository;
+
+@Service
+public class SongServiceImplementation implements SongService {
+
+	@Autowired
+	SongRepository repo;
+	@Override
+	public void addSong(Song song) {
+		// TODO Auto-generated method stub
+		repo.save(song);
+		
+	}
+	@Override
+	public List<Song> fetchAllSongs() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+	@Override
+	public boolean songExists(String name) {
+		// TODO Auto-generated method stub
+		Song song = repo.findByName(name);
+		if(song == null) {
+		return false;
+	}else {
+		return true;
+	}
+}
+	@Override
+	public void updateSong(Song song) {
+		// TODO Auto-generated method stub
+		repo.save(song);
+	}
+}
---
+++ src/main/java/com/example/demo/services/UsersServiceImplementation.java
@@ -0,0 +1,57 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Users;
+import com.example.demo.repositories.UsersRepository;
+
+@Service
+public class UsersServiceImplementation implements UsersService {
+    @Autowired
+	UsersRepository repo;
+	@Override
+	public String addUser(Users user) {
+		// TODO Auto-generated method stub
+		repo.save(user);
+		return "user added successfully";
+	}
+	@Override
+	public boolean emailExists(String email) {
+		// TODO Auto-generated method stub
+		if(repo.findByEmail(email)==null) {
+			return false;
+		}else {
+			return true;
+		}
+		
+	}
+	@Override
+	public boolean validateUser(String email, String password) {
+		// TODO Auto-generated method stub
+		Users user = repo.findByEmail(email);
+		String db_pass = user.getPassword();
+		if(password.equals(db_pass)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	@Override
+	public String getRole(String email) {
+		// TODO Auto-generated method stub
+		Users user = repo.findByEmail(email);
+		return user.getRole();
+	}
+	@Override
+	public Users getUser(String email) {
+		// TODO Auto-generated method stub
+		return repo.findByEmail(email);
+	}
+	@Override
+	public void updateUser(Users user) {
+		// TODO Auto-generated method stub
+		repo.save(user);
+	}
+
+}
